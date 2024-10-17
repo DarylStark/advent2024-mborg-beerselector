c60d93890a03143d3bdaf76bdf2074943d0e5a07
Daryl Stark <daryl@dstark.nl>
2024-10-17 15:49:20 +0200

    Better output

--- src/beer_selector/cli/model/normal/commands/external_license.cpp
+++ src/beer_selector/cli/model/normal/commands/external_license.cpp
@@ -3,10 +3,10 @@
 
 bool ExternalLicenseInfo::execute(std::map<std::string, std::string> args)
 {
-    _factory->get_output_handler()->println("\r\nExternal License Service Information");
+    _factory->get_output_handler()->println("\r\nExternal License Service Information\r\n");
     _factory->get_output_handler()->println("Received bytes: " + std::to_string(SerialLicenseRetriever::get_received_bytes()));
     _factory->get_output_handler()->println("Received codes:  " + std::to_string(SerialLicenseRetriever::get_received_codes()));
-    _factory->get_output_handler()->println("\r\n");
+    _factory->get_output_handler()->println("");
 
     return false;
 }
